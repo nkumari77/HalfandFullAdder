46cc0f965a71783f652f5551c7f3b07ed5cddb76
Zaheen Khusro <44664646+iamkhusro@users.noreply.github.com>
2019-10-20 23:46:31 +0800

    Update main.cpp

--- HalfandFullAdder/main.cpp
+++ HalfandFullAdder/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+using namespace std;
 int carry = 0;
 
 char halfadd(char bit1, char bit2){
@@ -16,20 +17,20 @@ char halfadd(char bit1, char bit2){
 
 int main(int argc, const char * argv[]) {
     int result;
-    std::cout<<"Half Adder"<<std::endl;
-    std::cout<<"=\t=\t=\t="<<std::endl;
-    std::cout<<"X\tY\tR\tC"<<std::endl;
-    std::cout<<"-\t-\t-\t-"<<std::endl;
+    cout<<"Half Adder"<<endl;
+    cout<<"=\t=\t=\t="<<endl;
+    cout<<"X\tY\tR\tC"<<endl;
+    cout<<"-\t-\t-\t-"<<endl;
     for(int i=0; i<2; i++){
         for(int j=0; j<2; j++){
             result = halfadd(i, j);
-            std::cout<<i<<"\t"<<j<<"\t"<<result<<"\t"<<carry<<std::endl;
+            cout<<i<<"\t"<<j<<"\t"<<result<<"\t"<<carry<<endl;
         }
     }
-    std::cout<<"\nFull Adder"<<std::endl;
-    std::cout<<"=\t=\t=\t=\t="<<std::endl;
-    std::cout<<"X\tY\tZ\tR\tC"<<std::endl;
-    std::cout<<"-\t-\t-\t-\t-"<<std::endl;
+    cout<<"\nFull Adder"<<endl;
+    cout<<"=\t=\t=\t=\t="<<endl;
+    cout<<"X\tY\tZ\tR\tC"<<endl;
+    cout<<"-\t-\t-\t-\t-"<<endl;
     
     int carry1 = carry;
     int carry2 = carry1 | carry;
@@ -40,7 +41,7 @@ int main(int argc, const char * argv[]) {
             for(int k=0; k<2; k++){
                 result = halfadd(i, j);
                 int sum = halfadd(k, result);
-                std::cout<<i<<"\t"<<j<<"\t"<<k<<"\t"<<sum<<"\t"<<carry<<std::endl;
+                cout<<i<<"\t"<<j<<"\t"<<k<<"\t"<<sum<<"\t"<<carry<<endl;
             }
         }
     }
