0aa92127dde2993b34bb477d4b33ec37e02beba9
ninjaCodes8523 <91782779+ninjaCodes8523@users.noreply.github.com>
2021-10-02 17:54:06 +0530

    Update HalfAddr.py

--- HalfAddr.py
+++ HalfAddr.py
@@ -1,5 +1,8 @@
 #Temp File for HAcktoberfest repo Delete it  ASAP
 print("HAck HAcktober fest")
 print("bishal Swain was here")
+x=6
+y=8
+print(x*y)
 print("processing....")
 print("click to go..")
