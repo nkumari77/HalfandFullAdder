6315ffc7c55c0d1908492aa2057b11a3d14faea6
subrat-chhaka <91790297+subrat-chhaka@users.noreply.github.com>
2021-10-02 19:37:24 +0530

    Update HalfAddr.py

--- HalfAddr.py
+++ HalfAddr.py
@@ -1,5 +1,9 @@
 #Temp File for HAcktoberfest repo Delete it  ASAP
 print("HAck HAcktober fest")
 print("bishal Swain was here")
+x=8
+y=3
+print(x+y)
+print(x-y)
 print("processing....")
 print("click to go..")
